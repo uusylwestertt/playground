e825bb05761afe4cfa5eb94f5f537680982fa707
sylwestercieslak <sylwester.cieslak@tsh.io>
2024-02-19 09:48:39 +0100

    Update log

--- src/pages/fights.ts
+++ src/pages/fights.ts
@@ -38,7 +38,7 @@ export class Fights extends BasePage {
     } else {
       for (let i = startCenter; i <= endCenter; i++) {
         if (await this.isVisibleElement(this.fightButtonCenter(i))) {
-          log.info(`Klikam w element w kolumnie right na pozycji:  ${i}`);
+          log.info(`Klikam w element w kolumnie center na pozycji:  ${i}`);
           await this.gameClick(this.fightButtonCenter(i));
           break;
         } else {
